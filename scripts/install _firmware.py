1802443481e5ef7667f9a2b70e14bafc20b30079
@BrianChege <123733116+CHEGEBB@users.noreply.github.com>
2023-12-22 13:35:29 +0300

    Update install _firmware.py

--- scripts/install _firmware.py
+++ scripts/install _firmware.py
@@ -1,91 +1,206 @@
 import os
+import sys
+import shutil
 import zipfile
-import urllib.request
-import user_interface
+import webbrowser
 
-# Define global variables
-firmware_path = ""
-firmware_zip_path = ""
+# Global vars
+unpack_dir = "updater_unpacked"
+zip_updater_script = "META-INF/com/google/android/updater-script"
+firmware_partition_map = {}
 
-# Function to download firmware file
-def download_firmware_file(firmware_url, firmware_zip_path, firmware_zip_name):
-    try:
-        # Create the firmware file path if it does not exist
-        if not os.path.exists(firmware_zip_path):
-            os.makedirs(firmware_zip_path)
 
-        # Download the firmware file
-        urllib.request.urlretrieve(firmware_url, os.path.join(firmware_zip_path, firmware_zip_name))
+def cleanup():
+    print("Cleaning up.")
+    shutil.rmtree(unpack_dir, ignore_errors=True)
 
-        # Check if the firmware file is valid
-        if check_firmware_file(os.path.join(firmware_zip_path, firmware_zip_name)):
-            return True
+
+def process_updater(updater_zip):
+    with zipfile.ZipFile(updater_zip, 'r') as zip_ref:
+        if zip_updater_script not in zip_ref.namelist():
+            print("This does not appear to be an android updater file.")
+            cleanup()
+            sys.exit(1)
+
+        os.makedirs(unpack_dir, exist_ok=True)
+        zip_ref.extractall(unpack_dir)
+
+
+def parse_firmware_partition_mapping():
+    with open(os.path.join(unpack_dir, zip_updater_script), 'r') as script_file:
+        lines = [line.strip() for line in script_file if
+                 'package_extract_file' in line and ('firmware-update' in line or 'RADIO' in line) and 'bak' not in line]
+        for line in lines:
+            filename, partition = [part.strip('",();') for part in line.replace('package_extract_file', '').split(',')]
+            firmware_partition_map[filename] = partition
+
+
+def sanity_check(firmware_base_dir):
+    total_num_files = 0
+    for dir_name in ['firmware-update', 'RADIO']:
+        dir_path = os.path.join(firmware_base_dir, dir_name)
+        num_files = len([f for f in os.listdir(dir_path) if os.path.isfile(os.path.join(dir_path, f))])
+        total_num_files += num_files
+
+    if total_num_files <= 0:
+        print("No firmware update files found.")
+        cleanup()
+        sys.exit(1)
+
+    if total_num_files != len(firmware_partition_map):
+        print("The number of firmware files found ({}) differs from what was found in the flashing script ({}).".format(
+            total_num_files, len(firmware_partition_map)))
+        print("Aborting.")
+        cleanup()
+        sys.exit(1)
+
+    for filename in firmware_partition_map.keys():
+        if not os.path.isfile(os.path.join(firmware_base_dir, filename)):
+            print("Failed to find firmware file {}.".format(filename))
+            cleanup()
+            sys.exit(1)
+
+
+def download_firmware_page(url):
+    print(f"Redirecting to the download page: {url}")
+    webbrowser.open(url)
+
+
+def create_firmware_directory(model):
+    model_directory = os.path.join(os.getcwd(), model)
+    os.makedirs(model_directory, exist_ok=True)
+    return model_directory
+
+
+def download_firmware(url, output_dir, output_filename):
+    print(f"Downloading firmware from {url}")
+    response = webbrowser.open(url)
+    print("\nIMPORTANT: Please download the firmware from the opened webpage.")
+    print(f"Once downloaded, unzip the firmware in the project directory: {output_dir}")
+    input("Press Enter when you have completed the download and unzip, then you can proceed with flashing.")
+    firmware_path = os.path.join(output_dir, output_filename)
+    if os.path.exists(firmware_path):
+        return firmware_path
+    else:
+        print(f"No firmware detected in {output_dir}. Aborting.")
+        sys.exit(1)
+
+
+def flash_firmware(model_directory, model):
+    print(f"\nFlashing firmware for {model}")
+    # Replace the following line with your actual flashing logic
+    print("Flashing logic goes here.")
+
+
+def select_phone_brand():
+    print("\nSelect a phone brand:")
+    print("1. Samsung")
+    print("2. Techno")  # Add more brands as needed
+    print("3. Back to main menu")
+
+    choice = input("Enter your choice (1-3): ").strip()
+
+    if choice == '1':
+        select_samsung_model()
+    elif choice == '2':
+        select_techno_model()
+    elif choice == '3':
+        return
+    else:
+        print("Invalid choice. Please enter a number between 1 and 3.")
+        select_phone_brand()
+
+
+def select_samsung_model():
+    print("\nSelect a Samsung model:")
+    print("1. Galaxy S10")
+    print("2. Galaxy Note 20")  # Add more models as needed
+    print("3. Back to phone brand selection")
+
+    choice = input("Enter your choice (1-3): ").strip()
+
+    if choice == '1':
+        download_and_flash_samsung_firmware("s10")
+    elif choice == '2':
+        download_and_flash_samsung_firmware("note20")
+    elif choice == '3':
+        select_phone_brand()
+    else:
+        print("Invalid choice. Please enter a number between 1 and 3.")
+        select_samsung_model()
+
+
+def select_techno_model():
+    print("\nSelect a Techno model:")
+    print("1. Camon 15")
+    print("2. Spark 5")  # Add more models as needed
+    print("3. Back to phone brand selection")
+
+    choice = input("Enter your choice (1-3): ").strip()
+
+    if choice == '1':
+        download_and_flash_techno_firmware("camon15")
+    elif choice == '2':
+        download_and_flash_techno_firmware("spark5")
+    elif choice == '3':
+        select_phone_brand()
+    else:
+        print("Invalid choice. Please enter a number between 1 and 3.")
+        select_techno_model()
+
+
+def download_and_flash_samsung_firmware(model):
+    # Example Samsung firmware download URL
+    samsung_firmware_url = "https://example.com/samsung/{}/firmware.zip".format(model)
+    model_directory = create_firmware_directory(model)
+    firmware_path = download_firmware(samsung_firmware_url, model_directory, "firmware.zip")
+    flash_firmware(model_directory, model)
+
+
+def download_and_flash_techno_firmware(model):
+    # Example Techno firmware download URL
+    techno_firmware_url = "https://www.hovatek.com/forum/forum-106.html".format(model)
+    model_directory = create_firmware_directory(model)
+    firmware_path = download_firmware(techno_firmware_url, model_directory, "firmware.zip")
+    flash_firmware(model_directory, model)
+
+
+def main():
+    print("FlashWizard - Firmware Installer")
+
+    while True:
+        print("\nChoose an option:")
+        print("1. Flash firmware")
+        print("2. Backup firmware")
+        print("3. Restore firmware")
+        print("4. Download firmware")
+        print("5. Select phone brand and model")
+        print("6. Quit")
+
+        choice = input("Enter your choice (1-6): ").strip()
+
+        if choice == '1':
+            select_phone_brand()
+        elif choice == '2':
+            backup_dir = input("Enter backup directory: ").strip()
+            updater_zip = input("Enter updater-zip file path: ").strip()
+            do_backup(backup_dir, updater_zip)
+        elif choice == '3':
+            backup_dir = input("Enter backup directory: ").strip()
+            updater_zip = input("Enter updater-zip file path: ").strip()
+            do_restore(backup_dir)
+        elif choice == '4':
+            url = input("Enter firmware download URL: ").strip()
+            select_phone_brand()
+        elif choice == '5':
+            select_phone_brand()
+        elif choice == '6':
+            print("Quitting.")
+            cleanup()
+            sys.exit(0)
         else:
-            # If the firmware file is not valid, delete the firmware file and return False
-            os.remove(os.path.join(firmware_zip_path, firmware_zip_name))
-            return False
-    except Exception as e:
-        print(f"Error downloading firmware: {e}")
-        return False
+            print("Invalid choice. Please enter a number between 1 and 6.")
 
-# Function to check if the firmware file is valid
-def check_firmware_file(firmware_zip_path):
-    try:
-        # Open the firmware file
-        with zipfile.ZipFile(firmware_zip_path, 'r') as firmware_zip_file:
-            # Get the firmware file list
-            firmware_zip_file_list = firmware_zip_file.namelist()
 
-        # Check if the firmware file list is empty
-        return len(firmware_zip_file_list) > 0
-    except Exception as e:
-        print(f"Error checking firmware file: {e}")
-        return False
-
-# Function to install firmware file
-def install_firmware_file(firmware_zip_path, firmware_install_ui):
-    try:
-        # Start the firmware installation process
-        firmware_install_ui.start()
-
-        # Check if the firmware file exists
-        if os.path.isfile(firmware_zip_path):
-            # Check if the firmware file is valid
-            if check_firmware_file(firmware_zip_path):
-                # Perform firmware installation here
-                # Add your code to extract and install firmware from the ZIP file
-
-                # For example, you can extract the contents of the ZIP file
-                with zipfile.ZipFile(firmware_zip_path, 'r') as firmware_zip_file:
-                    firmware_zip_file.extractall(firmware_path)
-
-                # Update UI or perform any additional installation steps
-
-            else:
-                # If the firmware file is not valid, delete the firmware file
-                os.remove(firmware_zip_path)
-                print("Invalid firmware file.")
-    except Exception as e:
-        print(f"Error installing firmware: {e}")
-    finally:
-        # Finish the firmware installation process
-        firmware_install_ui.finish()
-
-# Function to handle firmware installation process
-def handle_firmware_installation():
-    global firmware_path
-    global firmware_zip_path
-
-    firmware_url = user_interface.firmware_url
-    firmware_zip_name = user_interface.firmware_zip_name
-
-    firmware_path = user_interface.firmware_path
-    firmware_zip_path = user_interface.firmware_zip_path
-
-    firmware_install_ui = user_interface.FirmwareInstallUI()
-
-    if download_firmware_file(firmware_url, firmware_zip_path, firmware_zip_name):
-        install_firmware_file(os.path.join(firmware_zip_path, firmware_zip_name), firmware_install_ui)
-
-# Call the function to handle firmware installation
-handle_firmware_installation()
+if __name__ == "__main__":
+    main()
